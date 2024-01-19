ab3305805e34e56ed52d10efe61d9ce38b710ed9
Andrijana <andrijanab2000@gmail.com>
2024-01-19 20:17:47 +0100

    Remove comments

--- valgrind/massif/ms_print.sh
+++ valgrind/massif/ms_print.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 run_ms_print(){
-# Iterate over files matching the pattern
 for input_file in massif.out.*; do
-    # Extract some_number from the input file name
     pid=$(echo "$input_file" | sed 's/massif.out.//')
 
-    # Create the corresponding output file name
     output_file="massif_$pid.txt"
 
-    # Run the ms_print command
     ms_print "$input_file" > "$output_file"
     echo "Command executed for $input_file. Output written to $output_file."
 done
