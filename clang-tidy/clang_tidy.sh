54e35dcc78f1359d8ef3671a55145aa490a9712c
Andrijana <andrijanab2000@gmail.com>
2024-01-29 17:25:29 +0100

    Change clang-tidy script

--- clang-tidy/clang_tidy.sh
+++ clang-tidy/clang_tidy.sh
@@ -1,44 +1,16 @@
 #!/bin/bash
 
-backend_src="src"
-frontend_src="Main Window"
-
 backend_build_dir="backend/build"
 frontend_build_dir="frontend/src/lib/build"
 
 output_file_backend="output_backend.txt"
 output_file_frontend="output_frontend.txt"
 
-if [ -d "$backend_build_dir" ]; then
-    rm -rf $backend_build_dir
-fi
-
-if [ -d "$frontend_build_dir" ]; then
-    rm -rf $frontend_build_dir
-fi
-
-mkdir $backend_build_dir
 cd $backend_build_dir
-
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../$backend_src
-make > /dev/null
 
 run-clang-tidy -p . -header-filter=".*"  2>&1 | tee "../../$output_file_backend"
 
-cd ../..
+cd  ../../frontend/src/lib/
 
-mkdir $frontend_build_dir
-cd $frontend_build_dir 
-
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../"$frontend_src"
-make > /dev/null
-
-cd .. 
-
-run-clang-tidy -p build -header-filter=".*" "Main Window/*.cpp" "Main Window/*.h" "Main Window/fmt/*.h" "Main Window/fmt/*.cpp" "Chat/*.cpp" "Chat/*.h" "network_service/*.cpp" "network_service/*.hpp" 2>&1 | tee "../../../$output_file_frontend"
-
-cd ../../..
-
-rm -rf $backend_build_dir
-rm -rf $frontend_build_dir
+run-clang-tidy -p build -header-filter=".*" "Main Window/*.cpp" "Main Window/*.h" "Main Window/fmt/include/fmt*.h" "Main Window/fmt/src*.cc" "Chat/*.cpp" "Chat/*.h" "network_service/*.cpp" "network_service/*.hpp" 2>&1 | tee "../../../$output_file_frontend"
 
